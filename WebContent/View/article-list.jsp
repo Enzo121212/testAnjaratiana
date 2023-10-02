ee4749cf3228a7919bbac5d6eb70fc07a20eb7c4
Enzo <enzo@dssi.fr>
2023-10-02 16:26:30 +0300

    create servlet

---
+++ WebContent/View/article-list.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Liste des Articles</title>
+</head>
+<body>
+    <h1>Liste des Articles</h1>
+
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nom de l'Article</th>
+            <th>Actions</th>
+        </tr>
+        <c:forEach var="article" items="${articles}">
+            <tr>
+                <td>${article.id}</td>
+                <td>${article.article}</td>
+                <td>
+                    <a href="articles?action=edit&id=${article.id}">Modifier</a>
+                    <a href="articles?action=delete&id=${article.id}">Supprimer</a>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+
+    <h2>Ajouter/Modifier un Article</h2>
+
+    <form action="articles" method="post">
+        <input type="hidden" name="action" value="${article.id == null ? 'create' : 'update'}">
+        <input type="hidden" name="id" value="${article.id}">
+        Nom de l'Article: <input type="text" name="articleName" value="${article.article}">
+        <input type="submit" value="${article.id == null ? 'Ajouter' : 'Modifier'}">
+    </form>
+</body>
+</html>
+    
---
+++ src/com/kimiko/servlet/ArticleServlet.java
@@ -0,0 +1,125 @@
+package com.kimiko.servlet;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kimiko.beans.Article;
+import com.kimiko.dao.ArticleDAO;
+
+
+
+/**
+ * Servlet implementation class ArticleServlet
+ */
+@WebServlet(name = "articles", urlPatterns = { "/articles" })
+public class ArticleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	   private ArticleDAO articleDAO;
+
+	    public void init() {
+	        articleDAO = new ArticleDAO();
+	    }
+
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        String action = request.getParameter("action");
+	        if (action == null) {
+	            action = "list";
+	        }
+
+	        switch (action) {
+	            case "list":
+	                listArticles(request, response);
+	                break;
+	            case "edit":
+	                showEditForm(request, response);
+	                break;
+	            case "delete":
+	                deleteArticle(request, response);
+	                break;
+	            default:
+	                listArticles(request, response);
+	                break;
+	        }
+	    }
+
+	    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        String action = request.getParameter("action");
+	        if (action == null) {
+	            action = "list";
+	        }
+
+	        switch (action) {
+	            case "create":
+	                createArticle(request, response);
+	                break;
+	            case "update":
+	                updateArticle(request, response);
+	                break;
+	            default:
+	                listArticles(request, response);
+	                break;
+	        }
+	    }
+
+	    private void listArticles(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        List<Article> articles = articleDAO.getAllArticles();
+	       
+	        	
+	        for (Article T : articles) {
+                System.out.println( T.getArticle() );
+            }
+	        
+	        request.setAttribute("articles", articles);
+	        request.getRequestDispatcher("/WebContent/View/article-list.jsp").forward(request, response);
+	    }
+
+	    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        Long id = Long.parseLong(request.getParameter("id"));
+	        Article existingArticle = articleDAO.getArticleById(id);
+	        request.setAttribute("article", existingArticle);
+	        request.getRequestDispatcher("/WebContent/View/article-list.jsp").forward(request, response);
+	    }
+
+	    private void createArticle(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        String articleName = request.getParameter("articleName");
+	        Article newArticle = new Article(articleName);
+	        articleDAO.saveArticle(newArticle);
+	        response.sendRedirect("articles?action=list");
+	    }
+
+	    private void updateArticle(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        Long id = Long.parseLong(request.getParameter("id"));
+	        String articleName = request.getParameter("articleName");
+	        Article existingArticle = articleDAO.getArticleById(id);
+	        existingArticle.setArticle(articleName);
+	        articleDAO.updateArticle(existingArticle);
+	        response.sendRedirect("articles?action=list");
+	    }
+
+	    private void deleteArticle(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        Long id = Long.parseLong(request.getParameter("id"));
+	        Article articleToDelete = articleDAO.getArticleById(id);
+	        articleDAO.deleteArticle(articleToDelete);
+	        response.sendRedirect("articles?action=list");
+	    }
+
+	    public void destroy() {
+	        articleDAO.close();
+	    }
+
+}
